babe5252f29b01da9b040ead2a3515a1712f6c89
CGowdy <christiazn0321@gmail.com>
2014-09-29 16:03:07 -0500

    Framework Implemented

---
+++ src/com/cgowdy/game/state/MenuState.java
@@ -0,0 +1,47 @@
+package com.cgowdy.game.state;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import com.cgowdy.game.main.Resources;
+
+public class MenuState extends State {
+
+	@Override
+	public void init() {
+		System.out.println("Entered menu state.");
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(Resources.welcome, 0, 0, null);
+
+	}
+
+	@Override
+	public void onClick(MouseEvent e) {
+		System.out.println("Clicked the menu with " + e.getButton());
+
+	}
+
+	@Override
+	public void onKeyPress(KeyEvent e) {
+		System.out.println("Pressed " + e.getKeyChar() + " on main menu");
+
+	}
+
+	@Override
+	public void onKeyRelease(KeyEvent e) {
+		System.out.println("Released " + e.getKeyChar() + " on main menu");
+
+	}
+
+}
